93bfdcac150b54d4e0b27a660c79cc9deb334ebf
olegdayo <ol-sidorenkov@mail.ru>
2025-04-20 19:26:47 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
+)
+
+func main() {
+	logger := hclog.New(&hclog.LoggerOptions{
+		Level: hclog.Debug,
+	})
+
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: nil,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		logger.Error("plugin shutting down", "error", err)
+		os.Exit(1)
+	}
+}
